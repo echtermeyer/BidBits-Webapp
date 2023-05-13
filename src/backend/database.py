06b74e6d5ca579ae23d9b8b13bc8c2b6ce6e5844
acaero <davidschaefer42@gmail.com>
2023-05-13 19:40:41 +0200

    database dump

--- src/backend/database.py
+++ src/backend/database.py
@@ -3,29 +3,63 @@ import psycopg2
 
 from pathlib import Path
 
+# Datenbank 'bidbits' muss manuell angelegt werden bevor der dump durchgeführt werden kann.
 
-path_secrets = Path("src/secrets.json")
-with path_secrets.open() as f:
-    secrets = json.load(f)
+class Database:
+    def __init__(self):
+        self.__conn = None
+        self.__cur = None
+
+    def __connect(self):
+        path_secrets = Path("src/secrets.json")
+        with path_secrets.open() as f:
+            secrets = json.load(f)
+
+        self.__conn = psycopg2.connect(
+            dbname=secrets["POSTGRES"]["DBNAME"],
+            user=secrets["POSTGRES"]["USER"],
+            password=secrets["POSTGRES"]["PASSWORD"],
+            host=secrets["POSTGRES"]["HOST"],
+            port=secrets["POSTGRES"]["PORT"]
+        )
+
+        self.__cur = self.__conn.cursor()
+
+    def __disconnect(self):
+        self.__conn.commit()
+        self.__cur.close()
+        self.__conn.close()
+
+    def connection_manager(func):        
+        def wrapper(self, *args, **kwargs):
+            self.__connect()
+            func(self, *args, **kwargs)
+            self.__disconnect()
+
+        return wrapper
+
+    @connection_manager
+    def database_dump(self):
+        self.__cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+        table_names = self.__cur.fetchall()
+
+        if table_names.__len__() != 0:
+            print("Deleting current tables and domains")
+            for table in table_names:
+                self.__cur.execute(f'DROP TABLE "{table[0]}" CASCADE;')
+            self.__cur.execute("DROP DOMAIN IF EXISTS EMAIL")
+            self.__cur.execute("DROP DOMAIN IF EXISTS PAYMENT_TYPE")
+
+        print("Creating new tables")
+        with open("./src/backend/ddl.sql", "r") as sql_file:
+            sql = sql_file.read()
+            print(sql)
+            self.__cur.execute(sql)
+            
+        self.__cur.execute("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';")
+        print(f"\n == Successfully Created Tables == \n{[x[0] for x in self.__cur.fetchall()]}\n")
+  
 
 
-conn = psycopg2.connect(
-    dbname=secrets["POSTGRES"]["DBNAME"],
-    user=secrets["POSTGRES"]["USER"],
-    password=secrets["POSTGRES"]["PASSWORD"],
-    host=secrets["POSTGRES"]["HOST"],
-    port=secrets["POSTGRES"]["PORT"]
-)
-
-cur = conn.cursor()
-cur.execute("""SELECT * FROM user """)
-
-rows = cur.fetchall()
-if not len(rows):
-    print("Empty")
-
-for row in rows:
-    print(row)
-
-cur.close()
-conn.close()
+db = Database()
+db.database_dump()
---
+++ src/backend/ddl.sql
@@ -0,0 +1,127 @@
+-- Create Datatypes
+CREATE DOMAIN EMAIL AS VARCHAR
+    CHECK (VALUE LIKE '%@%.%');
+
+CREATE DOMAIN PAYMENT_TYPE AS VARCHAR
+    CHECK (value IN ('Paypal', 'Credit Card', 'Cash'));
+
+
+-- Create Category table
+CREATE TABLE Categorisation (
+    id SERIAL PRIMARY KEY,
+    category VARCHAR(50) NOT NULL,
+    subcategory VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Categorisation VALUES
+(1, 'Animals', 'Mammals'),
+(2, 'Animals', 'Amphibians'),
+(3, 'Antiquities', 'Boring old stuff'),
+(4, 'Antiquities', 'Cool old stuff');
+
+
+-- Create User table
+CREATE TABLE "user" (
+    username VARCHAR(255) PRIMARY KEY,
+    email EMAIL NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
+    name VARCHAR(100) GENERATED ALWAYS AS (firstName || ' ' ||lastName) STORED,
+    address TEXT NOT NULL,
+    phone VARCHAR(20) NOT NULL UNIQUE
+);
+
+INSERT INTO "user" (username, email, password, firstName, lastName, address, phone) VALUES
+('Karen', 'karen@astrology.com', 'godblessamerica', 'Karen', 'Nonyabis', '68161 Mannheim A1 14', '015789445571'),
+('Alfie', 'alfie@burminghambakery.com', 'nevergivepowertothebigman', 'Alfonso', 'Solomons', '24220 Burmingham Kensington Road 11', '01578331212'),
+('John', 'John.watson@gmail.com', 'thegameisafoot', 'John', 'Watson', '27615 London Baker Street 221b', '02295001922');
+
+
+-- Create Item table
+CREATE TABLE Item (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL,
+    startingPrice NUMERIC DEFAULT 0,
+    startTime TIMESTAMP NOT NULL,
+    endTime TIMESTAMP NOT NULL,
+    imageUrl TEXT NOT NULL,
+    user_username VARCHAR(50) REFERENCES "user"(username) 
+        ON DELETE CASCADE 
+        ON UPDATE CASCADE,
+    category_id INTEGER REFERENCES Categorisation(id) 
+        ON DELETE SET NULL
+);
+
+INSERT INTO Item VALUES
+(1, 'A cat with large ears', 'This cat is a great listener. Buy it now!', 20, '1999-01-08 04:05:06', '1999-01-10 04:05:06', 'https://en.wikipedia.org/wiki/Cat#/media/File:Kittyply_edit1.jpg', 'Karen', 1),
+(2, 'The Amber Room', 'It is golden, it is good. Found it under my bed.', 1200420123, '2023-05-13 06:07:08', '2023-05-30 06:07:08', 'https://de.wikipedia.org/wiki/Bernsteinzimmer#/media/Datei:Andrey_Zeest_-_Amber_Room_2_(autochrome).jpg', 'Alfie', 4),
+(3, 'A red bar stool', 'Good Chair, nice and comfy. Does not wobble.', 15, '2020-01-01 19:12:22', '2020-01-02 13:02:22', 'https://cdn.eichholtz.com/media/catalog/product/cache/62a0001ba384dd559aac2c6dd8434b29/1/1/114878_0_1_1.jpg', 'Karen', 3);
+
+
+-- Create Bid table
+CREATE TABLE Bid (
+    id SERIAL PRIMARY KEY,
+    amount NUMERIC NOT NULL,
+    bidTime TIMESTAMP NOT NULL,
+    user_username VARCHAR(50) REFERENCES "user"(username) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    item_id INTEGER REFERENCES Item(id) 
+        ON DELETE CASCADE
+);
+
+INSERT INTO Bid (amount, bidTime, user_username, item_id) VALUES
+(1200420124, '2023-05-13 09:10:11', 'Karen', 2),
+(1300000000, '2023-05-13 12:13:14', 'John', 2),
+(24, '1999-01-10 09:11:21', 'Alfie', 1);
+
+
+-- Create Watchlist table
+CREATE TABLE Watchlist (
+    user_username VARCHAR(50) REFERENCES "user"(username) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    item_id SERIAL REFERENCES Item(id) 
+        ON DELETE CASCADE
+);
+
+INSERT INTO Watchlist VALUES
+('Karen', 2),
+('John', 2),
+('Alfie', 1);
+
+
+-- Create Feedback table
+CREATE TABLE Feedback (
+    feedbackID SERIAL PRIMARY KEY,
+    rating INTEGER NOT NULL CHECK (rating BETWEEN 0 AND 10),
+    comment TEXT,
+    buyer VARCHAR(50) REFERENCES "user"(username) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    seller VARCHAR(50) REFERENCES "user"(username) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+INSERT INTO Feedback VALUES
+(1, 1, 'Karen sold me a wobbly chair, I am furious.', 'John', 'Karen'),
+(2, 10, 'The small gentleman bought my crooked bar stool. What a good lad.', 'Karen', 'John');
+
+
+-- Create Payment table
+CREATE TABLE Payment (
+    amount NUMERIC NOT NULL,
+    date TIMESTAMP NOT NULL,
+    type PAYMENT_TYPE NOT NULL,
+    user_username VARCHAR(50) REFERENCES "user"(username)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    item INTEGER REFERENCES Item(id) 
+        ON DELETE CASCADE
+);
+
+INSERT INTO Payment VALUES
+(42, '2020-01-02 13:02:22', 'Credit Card', 'John', 3);
